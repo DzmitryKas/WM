4d0b6e68d07fca1ec634a53534febf24440952ad
DzmitryKas <Kasperovich_dima@mail.ru>
2019-11-30 11:36:29 +0300

    feat: add scss loader, pug loader

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./scss/style.scss";
+
+let json = require('./data/dataset.json');
+// const dataFromFile = JSON.parse(dataset)
+console.log('json', json.page_meta.h1)
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const Fs = require('fs');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const HtmlWebpackPugPlugin = require('html-webpack-pug-plugin');
 
 const dataFromFile = JSON.parse(Fs.readFileSync('./src/data/dataset.json'))
 // console.log(dataFromFile);
@@ -14,20 +16,31 @@ module.exports = {
   devtool: 'source-map',
   module: {
     rules: [
-      // {
-      //   test: /\.js$/,
-      //   exclude: /(node_modules)/,
-      //   use: {
-      //     loader: 'babel-loader',
-      //   },
-      // },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
       },
+      {
+        test: /\.pug$/,
+        loader: 'pug-loader'
+      }
     ],
   },
-  plugins: [new HtmlWebpackPlugin({
-    chunks:	dataFromFile,
-  })],
+  plugins: [
+  new HtmlWebpackPlugin({
+    inject: false,
+    hash: true,
+    template: './src/index.pug',
+    filename: 'index.html'
+  }),
+  new ExtractTextPlugin({
+    filename: 'style.css'
+  }),
+  new HtmlWebpackPugPlugin()
+],
+
 };
